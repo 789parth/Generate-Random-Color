b6bf94f5c37044331b7e23f41f2db461e5fb362c
Parth Aadthakkar <parthadr@gmail.com>
2025-06-11 13:02:49 +0530

    Major Fixes

---
+++ app.js
@@ -0,0 +1,17 @@
+let btn = document.querySelector("button");
+
+btn.addEventListener("click", function () {
+    let h1 = document.querySelector("h1");
+    let color = getRandomColor();
+    h1.innerText = color;
+    let div = document.querySelector("div");
+    div.style.backgroundColor = color;
+});
+
+function getRandomColor() {
+    let red = Math.floor(Math.random() * 255);
+    let green = Math.floor(Math.random() * 255);
+    let blue = Math.floor(Math.random() * 255);
+    let color = `rgb(${red},${green},${blue})`;
+    return color;
+}
